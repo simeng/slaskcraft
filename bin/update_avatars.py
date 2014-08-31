f181d518d786c97370b89413d5f234f001a76ab3
Simen Graaten <simeng@gmail.com>
2014-08-31 09:46:42 +0000

    Switch to urllib2 which handles http status

--- bin/update_avatars.py
+++ bin/update_avatars.py
@@ -3,7 +3,7 @@
 
 import json
 import os
-import urllib
+import urllib2
 
 base_dir = os.path.dirname(__file__)
 
@@ -13,7 +13,11 @@ for player in players:
     nick = player['name']
     url = "https://minotar.net/helm/%s/32" % ( nick )
     print "Fetching %s" % ( url )
-    avatar = urllib.urlopen(url).read()
-    filename = "../www/img/helms/%s.png" % ( nick )
-    print "Writing %s" % ( filename )
-    open(os.path.join(base_dir, filename), "w+").write(avatar)
+    try:
+        avatar = urllib2.urlopen(url).read()
+        filename = "../www/img/helms/%s.png" % ( nick )
+        print "Writing %s" % ( filename )
+        open(os.path.join(base_dir, filename), "w+").write(avatar)
+    except urllib2.HTTPError, e:
+        print e.msg
+
