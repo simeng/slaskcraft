4e7cd0c5d585e1d095c53f6e020efb3993ba1958
Simen Graaten <simeng@gmail.com>
2014-10-20 15:24:14 +0000

    Skip images of Steve, he's too derpy for the website

--- bin/update_avatars.py
+++ bin/update_avatars.py
@@ -4,7 +4,9 @@
 import json
 import os
 import urllib2
+import md5
 
+steve_digest = "RJGwCWa0JfbvXSM3i2P4eg=="
 base_dir = os.path.dirname(__file__)
 
 players = json.loads(open(os.path.join(base_dir, "../www/json/stats/minecraft.slaskete.net/json/stats/whitelist.json")).read())
@@ -17,7 +19,11 @@ for player in players:
         avatar = urllib2.urlopen(url).read()
         filename = "../www/img/helms/%s.png" % ( nick )
         print "Writing %s" % ( filename )
-        open(os.path.join(base_dir, filename), "w+").write(avatar)
+        digest = md5.new(avatar).digest()
+        if digest.encode("base64").find(steve_digest) == 0:
+            print "Not saving Steve-avatar"
+        else:
+            open(os.path.join(base_dir, filename), "w+").write(avatar)
     except urllib2.HTTPError, e:
         print e.msg
 
