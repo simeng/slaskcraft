d2e4dd56472233edf2b76d1464843d5648a68f91
Simen Graaten <simeng@gmail.com>
2015-04-25 12:55:46 +0200

    Update player names list aswell in case someone has changed them

--- bin/update_avatars.py
+++ bin/update_avatars.py
@@ -9,10 +9,7 @@ import md5
 steve_digest = "RJGwCWa0JfbvXSM3i2P4eg=="
 base_dir = os.path.dirname(__file__)
 
-players = json.loads(open(os.path.join(base_dir, "../www/json/stats/minecraft.slaskete.net/json/stats/whitelist.json")).read())
-
-for player in players:
-    nick = player['name']
+def update_avatar(nick):
     url = "https://minotar.net/helm/%s/32" % ( nick )
     print "Fetching %s" % ( url )
     try:
@@ -26,4 +23,22 @@ for player in players:
             open(os.path.join(base_dir, filename), "w+").write(avatar)
     except urllib2.HTTPError, e:
         print e.msg
+
+def get_current_nick(uuid):
+    try:
+        jsondata = urllib2.urlopen("https://api.mojang.com/user/profiles/" + uuid + "/names").read()
+        names = json.loads(jsondata)
+        return names[-1]['name']
+    except urllib2.HTTPError, e:
+        print e.msg
+
+players = json.loads(open(os.path.join(base_dir, "../www/json/stats/minecraft.slaskete.net/json/stats/whitelist.json")).read())
+
+new_player_nicks = []
+
+for player in players:
+    update_avatar(player['name'])
+    new_player_nicks.append({'uuid': player['uuid'], 'name': get_current_nick(player['uuid'].replace("-", ""))})
+
+open(os.path.join(base_dir, "../www/json/players.json"), "w+").write(json.dumps(new_player_nicks))
 
